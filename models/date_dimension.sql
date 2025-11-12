e6a1ee8a5ee8e1787eaf9eefa9ccded39389078f
arifnyc@gmail.com <arifnyc@gmail.com>
2025-11-12 22:17:59 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,23 +1,13 @@
 with cte as (
 select
-TO_TIMESTAMP(started_at) as STARTED_AT,
-DATE(TO_TIMESTAMP(started_at)) as DATE_STARTED_AT,
-HOUR(TO_TIMESTAMP(started_at)) as HOUR_STARTED_AT,
-CASE
-WHEN DAYNAME(TO_TIMESTAMP(started_at)) IN ('Sat', 'Sun')
-    THEN 'WEEKEND'
-    ELSE 'BUSINESSDAY'
-END AS DAY_TYPE,
+TO_TIMESTAMP(started_at) AS STARTED_AT,
+DATE(TO_TIMESTAMP(started_at)) AS DATE_STARTED_AT,
+HOUR(TO_TIMESTAMP(started_at)) AS HOUR_STARTED_AT,
 
-CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2)
-    THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3,4,5)
-    THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8)
-    THEN 'SUMMER'
-    ELSE 'AUTUMN'
-END AS STATION_OF_YEAR
---DATE(STARTED_AT)
+{{day_type('STARTED_AT')}} AS DAY_TYPE,
+{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
+
+
 from 
 {{ source('demo', 'bike') }}    
 where started_at != 'started_at'
