65d96923a27bc6b31d54245f7e43627e027068cd
arifnyc@gmail.com <arifnyc@gmail.com>
2025-11-13 16:33:10 +0000

    stg_bike added

---
+++ models/stg_bike.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='table') }}
+
+select * from
+(
+with
+    bike as (
+        select
+            ride_id,
+            started_at,
+            ended_at,
+            start_station_name,
+            start_station_id,
+            end_station_name,
+            end_station_id,
+            start_lat,
+            start_lng,
+            end_lat,
+            end_lng,
+            member_csual
+        from {{ source("demo", "bike") }}
+    )
+select * from bike
+);
